d08ad6e186a65ac8470cf9d7952c8c045f51a26a
Marianne Rhaleigh G. Paradero <marianne.paradero@lpunetwork.edu.ph>
2025-09-11 15:20:18 +1000

    delete cd-digipic-backend and quiz fixed no more bugs

--- digipic-backend/backend/middleware/corsConfig.js
+++
@@ -1,8 +0,0 @@
-// ==== config/corsConfig.js ==== //
-const corsOptions = {
-  origin: ['http://127.0.0.1:5501', 'http://localhost:5501'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type'],
-  credentials: true,
-};
-module.exports = { corsOptions };
